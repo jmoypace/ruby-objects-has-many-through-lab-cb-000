658f3228dab4ce22e51fe4e0951ee23dd8f6c55f
jmoypace <jmoypace@gmail.com>
2018-07-19 03:00:23 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,18 @@
-class Appointment
-attr_accessor :patient, :doctor, :date
-def initialize(patient, doctor, date)
-  @patient=patient
-  @doctor=doctor
-  @date=date
 
-end
+class Appointment
+  attr_accessor :date, :doctor, :patient
+
+  @@all = []
+
+  def initialize(patient, doctor, date)
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
